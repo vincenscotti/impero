3b92aaf9a33709310fa55f86ff6acb2f8c060bbe
Vincenzo Scotti <vinc94@gmail.com>
2018-09-19 18:58:35 +0200

    Bugfix in watch.sh script

--- watch.sh
+++ watch.sh
@@ -3,7 +3,10 @@
 while inotifywait -r -e modify --exclude "(.*.db.*|.*.swp|build)" .; do
 	killall impero
 	sleep 0.2
-	cd templates/ &&
+
+	if [ $(basename `pwd`) != "templates" ]; then
+		cd templates/
+	fi
 	qtc &&
 	cd ../ &&
 	go build &&
